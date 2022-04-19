e6830f0f77de31db35ddd96d436abec1fe5df7e6
Ramzi Carter Jr <56376705+RamziCarter@users.noreply.github.com>
2022-04-18 23:02:00 -0500

    Add files via upload

---
+++ Methods/Methods/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Methods
+{
+    class MainClass
+    {
+
+        static void SingHappyBirthDay(String name, int age)
+        {
+           
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Happy BirthDay to you!");
+                Console.WriteLine("Happy BirthDay to you!");
+                Console.WriteLine("Happy BirthDay Dear " + name + "!");
+                Console.WriteLine("How old are you?\n"  + age + "!");
+                Console.WriteLine("Happy BirthDay to you!");
+            }
+
+        }
+
+        public static void Main(string[] args)
+        {
+
+            // A method performs a section of code when called or "invoked"
+            // This lets us reuse code without writing it multiple times
+
+            String name = "Ramzi";
+            int age = 35;
+            SingHappyBirthDay(name, age);
+         
+
+        }
+    }
+}
---
+++ ReturnKeyword/ReturnKeyword/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace ReturnKeyword
+{
+    class MainClass
+    {
+        static void namePicker(String name1r, String name2r, String name3r, int number)
+        {
+
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("The random name chosen is: ");
+
+           switch(number)
+            { 
+                case 1:
+                    Console.WriteLine(name1r);
+                    break;
+                case 2:
+                    Console.WriteLine(name2r);
+                    break;
+                case 3:
+                    Console.WriteLine(name3r);
+                    break;
+                default:
+                    break;
+            }
+
+        }
+
+
+        static String EndProgram(String EndNote)
+        {
+            return EndNote + " Termination Complete!";
+        }
+
+        public static void Main(string[] args)
+        {
+
+
+
+            Console.WriteLine("Enter the first name of your choice?");
+            String firstName = Console.ReadLine();
+
+            Console.WriteLine("Enter a second name of your choice?");
+            String secondName = Console.ReadLine();
+
+            Console.WriteLine("Enter a third name of your choice?");
+            String thridName = Console.ReadLine();
+
+
+            int min = 1;
+            int max = 4;
+
+            Random randNum = new Random();
+            int choice = randNum.Next(min, max);
+
+            namePicker(firstName, secondName, thridName, choice);
+            Console.WriteLine();
+
+            String e = "Ending Program....";
+            String finishStatement = EndProgram(e);
+            Console.WriteLine(finishStatement);
+
+
+        }
+    }
+}
