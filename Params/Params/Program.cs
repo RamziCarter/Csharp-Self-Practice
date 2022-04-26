7fb43893c46f62e5416bd490e239b1d3646c3c29
Ramzi Carter Jr <56376705+RamziCarter@users.noreply.github.com>
2022-04-25 23:02:43 -0500

    Add files via upload

---
+++ Params/Params/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Params
+{
+    class MainClass
+    {
+
+        static double checkoutTotal(params double [] prices)
+        {
+            double result = 0;
+
+            foreach(double price in prices)
+            {
+                result = price + result;
+            }
+
+            return result;
+        }
+
+        static String GetNames(params String [] names)
+        {
+            String person = "";
+            foreach (String people in names)
+            {
+                person = person + people + " ";
+            }
+
+            return person;
+        }
+
+        public static void Main(string[] args)
+        {
+            double result = checkoutTotal(2.59, 7.69, 8.99, 4, 99.98, 0.345, -25.1, 2);
+            Console.WriteLine(result);
+
+            String persons = GetNames("John", "Bill", "Derek", "Drake", "Yousif", "Alex");
+            Console.WriteLine(persons);
+        }
+    }
+}
