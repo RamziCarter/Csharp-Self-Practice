0608ae3ab607c2901e7b767cb8fc7ef29d6f6c1c
Ramzi Carter Jr <56376705+RamziCarter@users.noreply.github.com>
2022-04-27 09:52:08 -0500

    Add files via upload

---
+++ ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ExceptionHandling
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+
+            // try --> to execute code that could possibly have errors
+            // catch --> if error is found print exception
+            // finally --> always executes if error is caught or not!
+            try
+            {
+                Console.WriteLine("Enter a number: ");
+                int x = Convert.ToInt32(Console.ReadLine());
+
+                int power2 = x * x;
+                Console.Write($"{x} multipilied by {x} is: ");
+                Console.WriteLine(power2);
+
+            }
+
+
+            catch(FormatException e)
+            {
+                Console.WriteLine("You did not enter a whole number!");
+            }
+
+            finally
+            {
+                Console.WriteLine("Ending program...");
+            }
+
+        }
+    }
+}
